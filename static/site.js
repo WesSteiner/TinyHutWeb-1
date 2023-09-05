32179dbb6b7cf5eb7ccbd4d00b8bb3140aab365d
Wes Steiner <wwes@ksu.edu>
2023-09-05 17:51:46 -0500

    Just the cube moving around

--- static/site.js
+++ static/site.js
@@ -1,12 +1,11 @@
   var canvas = document.getElementById('mainimage');
   var ctx = canvas.getContext("2d");
-    var ballRadius = 10;
     var x = canvas.width/2;
     var y = canvas.height-30;
     var dx = 2;
     var dy = -2;
-    var paddleHeight = 5;
-    var paddleWidth = 5;
+    var paddleHeight = 15;
+    var paddleWidth = 15;
     var paddleX = (canvas.width-paddleWidth)/2;
     var paddleY = (canvas.height-paddleHeight)-30;
     var rightPressed = false;
@@ -72,16 +71,9 @@
       }
     }
 
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-    }
     function drawPaddle() {
         ctx.beginPath();
-        ctx.rect(paddleX, paddleY, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+        ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
@@ -89,37 +81,19 @@
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
         drawPaddle();
 
-        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-        }
-        if(y + dy < ballRadius) {
-            dy = -dy;
-        }
-        else if(y + dy > canvas.height-ballRadius) {
-            if(x > paddleX && x < paddleX + paddleWidth) {
-                dy = -dy;
-            }
-            /*else {
-                clearInterval(game);
-                alert("GAME OVER");
-                document.location.reload();
-            }*/
-        }
-
         if(rightPressed && paddleX < canvas.width-paddleWidth) {
-            paddleX += 7;
+            paddleX += 5;
         }
         else if(leftPressed && paddleX > 0) {
-            paddleX -= 7;
+            paddleX -= 5;
         }
         else if(downPressed && paddleY < canvas.height-paddleHeight) {
-          paddleY += 7;
+          paddleY += 5;
         }
         else if(upPressed && paddleY > 0) {
-          paddleY -= 7;
+          paddleY -= 5;
         }
 
         x += dx;
