19068c1abdcdc807250e94dde5c7e3dbce00f93d
Wes Steiner <wwes@ksu.edu>
2023-11-07 17:04:41 -0600

    Minor changes

---
+++ static/site.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('mainimage');
+const context = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
