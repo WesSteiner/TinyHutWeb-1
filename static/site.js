9021f5a2e3cc967332e76b71e34a7c493577c4b2
Wes Steiner <wwes@ksu.edu>
2023-11-14 11:33:20 -0600

    MainImage returns not canvas

--- static/site.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementById('mainimage');
-const context = canvas.getContext('2d');
-
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
-
-var mainimage = document.getElementById("mainimage");
